336b3a7af673239a45cdb13a35638ff40f4f1883
sanjay <sanjaykumar73189@gmail.com>
2022-10-26 15:53:46 +0530

    chat part completed

--- src/Chatcomponent/Chat.js
+++ src/Chatcomponent/Chat.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import "./Chat.css"
 import Chatbody from './Chatbody'
+import Chatfooter from './Chatfooter'
 import Chatheader from './Chatheader'
 function Chat() {
   return (
     <div className='chat'>
         <Chatheader/>
         <Chatbody/>
-        
+        <Chatfooter/>
     </div>
   )
 }
